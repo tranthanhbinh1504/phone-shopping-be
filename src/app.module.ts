13536fe45e30f384a21f3ba7716ad3cedc301241
tony <bbinhtony@gmail.com>
2022-05-30 16:15:06 +0700

    example todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TodoModule } from './todo/todo.module';
 
+const mongoose = MongooseModule.forRoot('mongodb://localhost/shopping');
 @Module({
-  imports: [],
+  imports: [mongoose, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })

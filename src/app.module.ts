aaa0a93584fb9fa1659d261bdd9343dbe2b1117b
tony <bbinhtony@gmail.com>
2022-05-31 19:40:44 +0700

    fix module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TodoModule } from './todo/todo.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ItemModule } from './item/item.module';
 
 const mongoose = MongooseModule.forRoot('mongodb://localhost/shopping');
 @Module({
-  imports: [mongoose, TodoModule, UserModule, AuthModule, ItemModule],
+  imports: [mongoose, UserModule, AuthModule, ItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
